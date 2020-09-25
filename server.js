8d2179de7a8bad9c896fe95be6756c12791aa4a3
Guilherme Mendes <guihenrique2806@gmail.com>
2020-09-25 11:08:14 -0300

    Criando a estrutura do nosso projeto

---
+++ server.js
@@ -0,0 +1,26 @@
+//importar as dependências
+const express = require('express')
+const bodyParser = require('body-parser')
+
+//inicializando o express
+const app = express()
+
+//configurar a view engine e configurar a pasta pública
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
+
+//configurar o body-parser
+app.use(bodyParser.urlencoded({extended:false}))
+app.use(bodyParser.json())
+
+//rotas
+app.get("/", (req,res) =>{
+    res.send("Vai etim!!!")
+})
+
+//escutar a porta
+app.listen(3000)
+
+if(true){
+    console.log('sdsdd')
+}

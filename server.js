8b96a10694fa73e8dfed2cf213f6bbb3a48908db
Guilherme Mendes <guihenrique2806@gmail.com>
2020-10-23 10:09:27 -0300

    Criando a estrutura do layout e estrutura dos avisos

--- server.js
+++ server.js
@@ -2,6 +2,9 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 
+//importando as rotas do aviso
+const routerAvisos = require('./components/avisos/AvisosController')
+
 //inicializando o express
 const app = express()
 
@@ -14,13 +17,7 @@ app.use(bodyParser.urlencoded({extended:false}))
 app.use(bodyParser.json())
 
 //rotas
-app.get("/", (req,res) =>{
-    res.send("Vai etim!!!")
-})
+app.use(routerAvisos)
 
 //escutar a porta
 app.listen(3000)
-
-if(true){
-    console.log('sdsdd')
-}

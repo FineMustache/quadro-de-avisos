1fcdd76ca094f4eacba2683706db3729fb21dfa3
Guilherme Mendes <guihenrique2806@gmail.com>
2020-11-13 11:13:16 -0300

    Cadastrar avisos

--- components/avisos/AvisosController.js
+++ components/avisos/AvisosController.js
@@ -1,15 +1,27 @@
 const router = require('express').Router()
 
-router.get('/',(req,res)=>{
+const Aviso = require('./Avisos')
+
+router.get('/', (req,res)=>{
   res.send("Pag Principal")
 })
 
-router.get('/avisos',(req,res)=>{
+router.get('/avisos', (req,res)=>{
   res.send("Avisos Cadastrados")
 })
 
-router.get('/avisos/novo',(req,res)=>{
+router.get('/avisos/novo', (req,res)=>{
   res.render('formulario_avisos')
+})
+
+router.post('/avisos/novo', async (req, res)=>{
+  const titulo = req.body.titulo
+  const data = req.body.data
+  const mensagem = req.body.mensagem
+
+  const msg = await Aviso.salvar({titulo, data, mensagem})
+  res.render('formulario_avisos', {msg})
+
 })
 
 module.exports = router
--- knexfile.js
+++ knexfile.js
@@ -1,9 +1,12 @@
+const dotenv = require('dotenv')
+dotenv.config()
+
 const knex = require('knex')({
   client: 'mysql2',
   connection: {
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
-    password: process.env.DB_PASS,
+    password: process.env.DB_PASS ,
     database: process.env.DB_DATABASE
   }
 })

f6fdeb9a2f0ae0107c0f6f96c08428754439fda2
Guilherme Mendes <guihenrique2806@gmail.com>
2020-12-07 15:36:37 -0300

    Criando o menu Superior

--- components/avisos/AvisosController.js
+++ components/avisos/AvisosController.js
@@ -1,13 +1,17 @@
 const router = require('express').Router()
 
+const Avisos = require('./Avisos')
 const Aviso = require('./Avisos')
 
 router.get('/', (req,res)=>{
   res.send("Pag Principal")
 })
 
-router.get('/avisos', (req,res)=>{
-  res.send("Avisos Cadastrados")
+router.get('/avisos', async (req,res)=>{
+  //res.send("Avisos Cadastrados")
+  const avisos = await Avisos.selecionarTodos()
+  //res.send(avisos)
+  res.render('avisos')
 })
 
 router.get('/avisos/novo', (req,res)=>{
